af4f0d13fa4b740e93f078f838d4ae18b55991ee
Somdotta07 <jhilick.s@gmail.com>
2022-01-25 15:34:47 +0100

    set up associations between objects

--- person.rb
+++ person.rb
@@ -1,14 +1,29 @@
+require_relative './corrector'
+require_relative './rental'
+
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rental
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.ran(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rental = []
   end
-  private :of_age?
+
+  def validate_name
+    @name = @corrector.correct_name(name)
+  end
+
+  def add_rental(book, date)
+    Rental.new(book, date, self)
+  end
+
+  private
+
   def of_age?
     @age >= 18
   end
