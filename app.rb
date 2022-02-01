0f55983a4d277a543d49bb9b00b6e1d0f945d40e
Somdotta07 <jhilick.s@gmail.com>
2022-02-01 11:34:42 +0100

    Add changes

--- app.rb
+++ app.rb
@@ -32,27 +32,35 @@ class App
   end
 
   def list_all_books
+    puts 'There are no books yet! Kindly add books.' if @books.empty?
+
     @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     sleep 0.75
   end
 
   def list_all_people
+    puts 'There are no people yet! Kindly add a student or teacher.' if @people.empty?
     @people.map { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
     sleep 0.75
   end
 
-  def create_person
-    print 'Do you want to create a student(1) or a teacher(2)? [Input the number]:'
-    input = gets.chomp.to_i
+  def person_details
     print 'Age: '
     age = gets.chomp.to_i
 
     print 'Name: '
     name = gets.chomp
-    case input
-    when 1
+    [age, name]
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or teacher (2) [Input a number]: '
+    option = gets.chomp
+    age, name = person_details
+    case option
+    when '1'
       create_student(age, name)
-    when 2
+    when '2'
       create_teacher(age, name)
     end
     puts 'Person created successfully!'
@@ -78,45 +86,57 @@ class App
     sleep 0.75
   end
 
-  def create_book
+  def book_details
     print 'Title: '
     title = gets.chomp
 
     print 'Author: '
     author = gets.chomp
+    [title, author]
+  end
 
+  def create_book
+    title, author = book_details
     @books << Book.new(title, author)
 
     puts 'Book added successfully'
     sleep 0.75
   end
+end
 
-  def create_rental
-    puts 'Select a book from the following list by number'
-    @books.each_with_index { |book, index| puts "#{index}) #{book}" }
+def rental_details
+  puts 'Select a book from the following list by number'
+  @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
 
-    book_id = gets.chomp.to_i
+  book_id = gets.chomp.to_i
 
-    puts 'Select a person from the following list by number (not id)'
-    @people.each_with_index { |person, index| puts "#{index}) #{person}" }
-
-    person_id = gets.chomp.to_i
-
-    print 'Date: '
-    date = gets.chomp.to_s
-
-    @rentals << Rental.new(date, @people[person_id], @books[book_id])
-
-    puts 'Rental created successfully'
-    sleep 0.75
+  puts 'Select a person from the following list by number (not id)'
+  @people.each_with_index do |person, index|
+    puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
   end
 
-  def list_rentals_by_person_id
-    print 'ID of person: '
-    id = gets.chomp.to_i
+  person_id = gets.chomp.to_i
 
-    puts 'Rentals:'
-    @rentals.each { |rental| puts rental if rental.person.id == id }
-    sleep 0.75
+  print 'Date: '
+  date = gets.chomp.to_s
+  [book_id, person_id, date]
+end
+
+def create_rental
+  book_id, person_id, date = rental_details
+  @rentals << Rental.new(date, @people[person_id], @books[book_id])
+
+  puts 'Rental created successfully'
+  sleep 0.75
+end
+
+def list_rentals_by_person_id
+  print 'ID of person: '
+  id = gets.chomp.to_i
+
+  puts 'Rentals:'
+  @rentals.each do |rental|
+    puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
   end
+  sleep 0.75
 end
